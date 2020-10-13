15575b72070f10626091239f43f09aa308d17548
Destanee Glenn <destanee.glenn@gmail.com>
2020-10-12 20:36:57 -0500

    creating department seeds

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+-- Insert a set of records into department.
+INSERT INTO department (id, name) VALUES (90, "");
+INSERT INTO department (id, name) VALUES (80, "");
+INSERT INTO department (id, name) VALUES (20, "");
+INSERT INTO department (id, name) VALUES (70, "");
+
