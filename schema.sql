ef7093c16b2986917fb921761b0705a7793cf837
Destanee Glenn <destanee.glenn@gmail.com>
2020-10-12 20:35:29 -0500

    creating department table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,12 @@
 -- Create the database managmentSystem and specified it for use.
 DROP DATABASE IF EXISTS
 CREATE DATABASE managmentSystem;
+
+USE managmentSystem;
+
+-- Create the table department.
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
