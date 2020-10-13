b3b6be30edb61d9dfe7ba05574642fcceab7647e
Destanee Glenn <destanee.glenn@gmail.com>
2020-10-12 20:37:49 -0500

    creating employee seeds

--- seeds.sql
+++ seeds.sql
@@ -10,3 +10,11 @@ INSERT INTO role (id, title, salary, department) VALUES (90, "", .111, 1);
 INSERT INTO role (id, title, salary, department) VALUES (80, "", .111, 1);
 INSERT INTO role (id, title, salary, department) VALUES (20, "", .111, 1);
 INSERT INTO role (id, title, salary, department) VALUES (70, "", .111, 1);
+
+-- Insert a set of records into employee.
+INSERT INTO employee (id, firstName, lastName, roleId, managerId) VALUES (90, "", "",1, 1);
+INSERT INTO employee (id, firstName, lastName, roleId, managerId) VALUES (80, "", "",1, 1);
+INSERT INTO employee (id, firstName, lastName, roleId, managerId) VALUES (20, "", "",1, 1);
+INSERT INTO employee (id, firstName, lastName, roleId, managerId) VALUES (70, "", "",1, 1);
+
+
