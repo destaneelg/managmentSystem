2b3b576a7d344a836ae94978e829eb04d9243b9d
Destanee Glenn <destanee.glenn@gmail.com>
2020-10-12 20:37:24 -0500

    creating role seeds

--- seeds.sql
+++ seeds.sql
@@ -4,3 +4,9 @@ INSERT INTO department (id, name) VALUES (80, "");
 INSERT INTO department (id, name) VALUES (20, "");
 INSERT INTO department (id, name) VALUES (70, "");
 
+
+-- Insert a set of records into role.
+INSERT INTO role (id, title, salary, department) VALUES (90, "", .111, 1);
+INSERT INTO role (id, title, salary, department) VALUES (80, "", .111, 1);
+INSERT INTO role (id, title, salary, department) VALUES (20, "", .111, 1);
+INSERT INTO role (id, title, salary, department) VALUES (70, "", .111, 1);
